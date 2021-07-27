59a46c26195ae2f5afc39fa28f210167597460b4
jiawei <jiawei@iscas.ac.cn>
2021-07-27 11:52:34 +0800

    Fix regs reload  passes in -O2 casue fp regs used issue

--- gcc/config/riscv/riscv.c
+++ gcc/config/riscv/riscv.c
@@ -2478,7 +2478,7 @@ riscv_function_arg_boundary (machine_mode mode, const_tree type)
 static unsigned
 riscv_pass_mode_in_fpr_p (machine_mode mode)
 {
-  if (GET_MODE_UNIT_SIZE (mode) <= UNITS_PER_FP_ARG)
+  if (GET_MODE_UNIT_SIZE (mode) <= UNITS_PER_FP_ARG && !TARGET_ZFINX)
     {
       if (GET_MODE_CLASS (mode) == MODE_FLOAT)
 	return 1;
@@ -4808,10 +4808,8 @@ riscv_conditional_register_usage (void)
 
   if (!TARGET_HARD_FLOAT)
     {
-      if(!TARGET_ZFINX){
-         for (int regno = FP_REG_FIRST; regno <= FP_REG_LAST; regno++)
-	   fixed_regs[regno] = call_used_regs[regno] = 1;
-      }
+      for (int regno = FP_REG_FIRST; regno <= FP_REG_LAST; regno++)
+	fixed_regs[regno] = call_used_regs[regno] = 1;
     }
 
   /* In the soft-float ABI, there are no callee-saved FP registers.  */
